faa60229f697c88a60f2d4ec238b76410811ca04
ldevuono <lauradevuono@gmail.com>
2023-04-23 11:32:32 -0400

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Tweeter</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h1>Wild Flour Bakery</h1>
     </>
   )
 }
